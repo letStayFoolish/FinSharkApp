a7c7bcedf854583bda9123011ce20b075ba31f4f
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-06-28 13:45:21 +0200

    Add Comment repository, controller, DTO, and mapping
    - Implement `ICommentRepository` interface and its `CommentRepository` implementation
    - Add `CommentController` with `GetAllComments` endpoint
    - Introduce `CommentDto` for transferring comment data
    - Create `CommentMapper` for mapping `Comment` model to `CommentDto`
    - Register `ICommentRepository` in dependency injection container

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using api.Interfaces;
+using api.Mappers;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[Route("api/comment")]
+[ApiController]
+public class CommentController : ControllerBase
+{
+  private readonly ICommentRepository _commentRepository;
+
+  public CommentController(ICommentRepository commentRepository)
+  {
+    _commentRepository = commentRepository;
+  }
+
+  [HttpGet] // read all comments
+  public async Task<IActionResult> GetAllComments()
+  {
+    var comments = await _commentRepository.GetAllAsync();
+    // DTO Comments...
+    var commentsDto = comments.Select(c => c.ToCommentDto());
+    return Ok(commentsDto);
+  }
+
+}
--- api/Program.cs
+++ api/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllers();
 // Configures the context to use Microsoft SQL Server - search through our appsettings.json.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
