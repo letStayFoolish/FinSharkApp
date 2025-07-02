75a819f9af7299f2a0f87b3e80ec99247c8e3397
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-07-02 08:56:37 +0200

    Refactor models and JWT configuration for improved readability and null-safety
    - Add null-forcing operator `!` to JWT SigningKey retrieval in `Program.cs` to ensure stronger type safety.
    - Update `AppUser`, `Stock` models to initialize lists as empty by default, aligning with clean code practices.
    - Remove unused `Risk` property from `AppUser` for a cleaner model definition.

--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -5,5 +5,5 @@ namespace api.Models;
 public class AppUser : IdentityUser
 {
   // public int Risk { get; set; }
-  public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
+  public List<Portfolio> Portfolios { get; } = [];
 }
--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -12,7 +12,6 @@ public class Stock
   [Column(TypeName = "decimal(18, 2)")] public decimal LastDiv { get; set; }
   public string Industry { get; set; } = string.Empty;
   public long MarketCap { get; set; }
-  public List<Comment> Comments { get; set; } = new List<Comment>();
-  public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
-
+  public List<Comment> Comments { get; set; } = [];
+  public List<Portfolio> Portfolios { get; set; } = [];
 }
