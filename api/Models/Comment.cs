3b7bfc4107fe2bb589dd7f4386f9bb5116710bb0
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-07-02 13:11:36 +0200

    Enforce immutability and null-safety in models
    - Update `Stock` and `Comment` models to use `required` properties for stronger null-safety.
    - Convert `Id` in `Stock` to `init` for immutability.

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -6,8 +6,8 @@ namespace api.Models;
 public class Comment
 {
   public int Id { get; set; }
-  public string Title { get; set; } = string.Empty;
-  public string Content { get; set; } = string.Empty;
+  public required string Title { get; set; }
+  public required string Content { get; set; }
   public DateTime CreatedOn { get; set; } = DateTime.Now;
   public int? StockId { get; set; } // Navigation property!!!
   public Stock? Stock { get; set; } //
--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -5,8 +5,8 @@ namespace api.Models;
 [Table("Stocks")]
 public class Stock
 {
-  public int Id { get; set; }
-  public string Symbol { get; set; } = string.Empty;
+  public int Id { get; init; }
+  public required string Symbol { get; set; }
   public string CompanyName { get; set; } = string.Empty;
   [Column(TypeName = "decimal(18, 2)")] public decimal Purchase { get; set; }
   [Column(TypeName = "decimal(18, 2)")] public decimal LastDiv { get; set; }
